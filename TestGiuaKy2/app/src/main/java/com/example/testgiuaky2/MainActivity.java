a13d644d52cc4a09fa6cc0f3191da80abc1cd70a
tudao164 <daothanhtu2018@gmail.com>
2025-03-21 09:35:55 +0700

    First commit

---
+++ TestGiuaKy2/app/src/main/java/com/example/testgiuaky2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.testgiuaky2;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnStart;
+    private TextView txtUserInfo;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        txtUserInfo = findViewById(R.id.txtUserInfo);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE);
+        String userEmail = sharedPreferences.getString("email", "Chưa đăng nhập");
+
+        txtUserInfo.setText("Email: " + userEmail);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
